a81b04c43c6ffb4847e36f66b72fd6d8facea369
corey-marchand <corey.marchand@me.com>
2020-05-12 16:54:08 -0700

    lucas function complete

--- math_series/series.py
+++ math_series/series.py
@@ -19,3 +19,21 @@ def fibonacci(n):
 
 print(fibonacci(9))
 
+def lucas(n):
+    a = [2, 1]
+    if n < 0:
+        print('Incorrect Input')
+    elif n == 0:
+        return a[0]
+    elif n == 1:
+        return a[1]
+    else: 
+        for i in range(2,n+1):
+            c = a[0] + a[1]
+            a[0] = a[1]
+            a[1] = c 
+
+        return a[1]
+    
+print(lucas(7))
+
