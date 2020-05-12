0c0d4a49a685e492f04319118799e88ecc175e27
corey-marchand <corey.marchand@me.com>
2020-05-12 16:39:09 -0700

    fibonacci function complete

---
+++ math_series/series.py
@@ -0,0 +1,21 @@
+__version__ = '0.1.0'
+
+def fibonacci(n):
+    a = [0, 1]
+    if n < 0:
+        print('Incorret input')
+    elif n == 0:
+        return a[0]
+    elif n == 1:
+        return a[1]
+    else:
+        for i in range(2,n+1):
+            c = a[0] + a[1]
+            a[0] = a[1]
+            a[1] = c
+
+        return a[1]
+
+
+print(fibonacci(9))
+
