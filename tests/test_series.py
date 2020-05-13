f73527d103b6f43a19c41e280fa4b42f63821a63
corey-marchand <corey.marchand@me.com>
2020-05-12 17:30:39 -0700

    testing for fibonacci and lucas complete

--- tests/test_series.py
+++ tests/test_series.py
@@ -1,5 +1,43 @@
-from math_series import __version__
+from math_series.series import fibonacci
+from math_series.series import lucas
+from math_series.series import sum_series 
 
+def test_fibonacci_one():
+    actual = fibonacci(1)
+    expected = 1
+    assert actual == expected
 
-def test_version():
-    assert __version__ == '0.1.0'
+def test_fibonacci_two():
+    actual = fibonacci(0)
+    expected = 0
+    assert actual == expected
+
+def test_fibonacci_three():
+    actual = fibonacci(2)
+    expected = 1
+    assert actual == expected
+
+def test_fibonacci_four():
+    actual = fibonacci(3)
+    expected = 2
+    assert actual == expected
+
+def test_lucas_one():
+    actual = lucas(0)
+    expected = 2
+    assert actual == expected
+
+def test_lucas_two():
+    actual = lucas(1)
+    expected = 1
+    assert actual == expected
+
+def test_lucas_three():
+    actual = lucas(2)
+    expected = 3
+    assert actual == expected
+
+def test_lucas_four():
+    actual = lucas(4)
+    expected = 7
+    assert actual == expected
